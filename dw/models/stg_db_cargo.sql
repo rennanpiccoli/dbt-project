efdc0675eb069df832a9d7135a5983f15b2198a6
Rennan Piccoli <rennan.piccoli@lovys.com>
2026-01-25 05:58:18 -0300

    changed stg_db_cargo

--- dw/models/stg_db_cargo.sql
+++ dw/models/stg_db_cargo.sql
@@ -1,7 +1,13 @@
 with 
 
 base as (
-    select * from {{source('datalake', 'db_cargo_raw')}}
+    select 
+        CD_CARGO as id_cargo,
+        CD_DEPTO as cd_dpto,
+        DS_CARGO as dsc_cargo,
+        ST_CARGO as dsc_status_cargo
+    
+    from {{source('datalake', 'db_cargo_raw')}}
 )
 
 select * from base
