8e9a8a1b5e9bd93fff434a030e004b4985f8babd
rod <rodrigoefernandes@gmail.com>
2026-02-21 10:26:59 +0000

    Created staging models

---
+++ dw/models/stg_db_bairro.sql
@@ -0,0 +1,12 @@
+with 
+
+base as (
+    select 
+        CD_BAIRRO as id_bairro,
+        CD_CIDADE as cod_cidade,
+        NM_BAIRRO as dsc_bairro,
+    
+    from {{source('datalake', 'db_bairro_raw')}}
+)
+
+select * from base
